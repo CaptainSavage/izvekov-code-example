dbbbfb666195a44aa93a602dc1307666cb63d700
Ilya Izvekov <i.izvekov@alfa2b.ru>
2024-11-19 23:04:21 +0300

    PHPDoc update

--- basic/models/DictProject.php
+++ basic/models/DictProject.php
@@ -4,6 +4,12 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * This is the model class for table "{{%dict_project}}".
+ *
+ * @property integer $id
+ * @property string $name_full
+ */
 class DictProject extends ActiveRecord
 {
     public static function tableName(): string
